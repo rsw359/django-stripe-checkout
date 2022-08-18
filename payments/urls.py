dac0529956b3af89d7b0b5f4dbbaa271e901dfa7
Roger Wells <rsw359@gmail.com>
2022-08-18 14:20:01 +0900

    checkout session views and url added, xhr handler next

--- payments/urls.py
+++ payments/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.HomePageView.as_view(), name='home'),
   path('config/', views.stripe_config),
+  path('create_checkout_session/', views.create_checkout_session),
 ]
