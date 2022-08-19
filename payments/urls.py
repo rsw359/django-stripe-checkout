2013443ff9ee12cd7f5e0a3ed1c37b17b3b824ed
Roger Wells <rsw359@gmail.com>
2022-08-19 22:49:09 +0900

    checkout session complete, 404 fixed in URLS

--- payments/urls.py
+++ payments/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.HomePageView.as_view(), name='home'),
   path('config/', views.stripe_config),
-  path('create_checkout_session/', views.create_checkout_session),
+  path('create-checkout-session/', views.create_checkout_session),
 ]
