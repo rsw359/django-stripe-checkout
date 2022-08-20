02d089be98b81d78a826aae18d8497094079d323
Roger Wells <rsw359@gmail.com>
2022-08-20 10:33:27 +0900

    redirects complete, payments processed

--- payments/urls.py
+++ payments/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
   path('', views.HomePageView.as_view(), name='home'),
   path('config/', views.stripe_config),
   path('create-checkout-session/', views.create_checkout_session),
+  path('success', views.SuccessView.as_view()),
+  path('cancelled', views.CancelledView.as_view()),
 ]
